50c4446cbfaac3e565bb76806468915902ef2af4
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2024-02-25 13:53:06 +0000

    Begin work on OCS building

--- build.sh
+++ build.sh
@@ -1,14 +1,55 @@
 #!/bin/bash
 
-BASE_IMAGE_REGISTRY=quay.io/okderators
-CHANNEL=alpha
-DEFAULT_CHANNEL=alpha
+NAMESPACE=${NAMESPACE:-okd-team}
+BASE_IMAGE_REGISTRY=${BASE_IMAGE_REGISTRY:-quay.io/okderators}
+CHANNEL=${CHANNEL:-dev}
+DEFAULT_CHANNEL=${DEFAULT_CHANNEL:-stable}
+VERSION=${VERSION:-dev}
+ENV_MAP=${ENV_MAP:-""}
+BUILD_IMAGE=${BUILD_IMAGE:-quay.io/okderators/bundle-tools:vdev}
+ENABLE_TIMESTAMP=${ENABLE_TIMESTAMP:-true}
+
+function build_operand() {
+  NAME=$3
+  tkn pipeline start operand \
+    --param repo-url=$1 \
+    --param repo-ref=$2 \
+    --param base-image-registry=$BASE_IMAGE_REGISTRY \
+    --param image-name=$3 \
+    --param image-version=$VERSION \
+    --param build-image $BUILD_IMAGE \
+    --param enable-timestamp=$ENABLE_TIMESTAMP \
+    --param env-map=$ENV_MAP \
+    --workspace name=workspace,claimName=$NAME-volume \
+    --workspace name=patches,config=$5-patch \
+    --pod-template pod-template.yaml \
+    -n $NAMESPACE
+}
+
+function build_operator() {
+  NAME=$3
+  tkn pipeline start operator \
+    --param repo-url=$1 \
+    --param repo-ref=$2 \
+    --param base-image-registry=$BASE_IMAGE_REGISTRY \
+    --param image-name=$3 \
+    --param image-version=$VERSION \
+    --param channel=$CHANNEL \
+    --param default-channel=$DEFAULT_CHANNEL \
+    --param build-image $BUILD_IMAGE \
+    --param enable-timestamp=$ENABLE_TIMESTAMP \
+    --param env-map=$ENV_MAP \
+    --workspace name=workspace,claimName=$NAME-volume \
+    --workspace name=patches,config=$NAME-patch \
+    --pod-template pod-template.yaml \
+    -n $NAMESPACE
+}
 
 case $1 in
   "bundle-tools")
     tkn pipeline start image-build \
-      --param repo-url=https://github.com/upstream-operators/okd-operator-pipeline \
-      --param repo-ref=main \
+      --param repo-url=https://github.com/okd-project/okd-operator-pipeline \
+      --param repo-ref=ocs \
       --param base-image-registry=$BASE_IMAGE_REGISTRY \
       --param image-name=bundle-tools \
       --param image-version=dev \
@@ -18,42 +59,13 @@ case $1 in
       -n okd-team
     ;;
   "gitops-console-plugin")
-    tkn pipeline start operand \
-      --param repo-url=https://github.com/redhat-developer/gitops-console-plugin \
-      --param repo-ref=main \
-      --param base-image-registry=$BASE_IMAGE_REGISTRY \
-      --param image-name=gitops-console-plugin \
-      --param image-version=dev \
-      --workspace name=workspace,claimName=gitops-console-plugin-volume \
-      --workspace name=patches,config=gitops-console-plugin-patch \
-      --pod-template pod-template.yaml \
-      -n okd-team
+    build_operand https://github.com/redhat-developer/gitops-console-plugin main gitops-console-plugin
     ;;
   "gitops-backend")
-    tkn pipeline start operand-golang \
-      --param repo-url=https://github.com/redhat-developer/gitops-backend \
-      --param repo-ref=master \
-      --param base-image-registry=$BASE_IMAGE_REGISTRY \
-      --param image-name=gitops-backend \
-      --param image-version=dev \
-      --workspace name=workspace,claimName=gitops-backend-volume \
-      --workspace name=patches,config=gitops-backend-patch \
-      --pod-template pod-template.yaml \
-      -n okd-team
+    build_operand https://github.com/redhat-developer/gitops-backend master gitops-backend
     ;;
   "gitops-operator")
-    tkn pipeline start operator-golang \
-      --param repo-url=https://github.com/redhat-developer/gitops-operator \
-      --param repo-ref=master \
-      --param base-image-registry=$BASE_IMAGE_REGISTRY \
-      --param image-name=gitops-operator \
-      --param image-version=dev \
-      --param channel=$CHANNEL \
-      --param default-channel=$DEFAULT_CHANNEL \
-      --workspace name=workspace,claimName=gitops-operator-volume \
-      --workspace name=patches,config=gitops-operator-patch \
-      --pod-template pod-template.yaml \
-      -n okd-team
+    build_operator https://github.com/redhat-developer/gitops-operator master gitops-operator
     ;;
   "noobaa-core")
     tkn pipeline start operand-noobaa \
@@ -68,45 +80,19 @@ case $1 in
       -n okd-team
     ;;
   "noobaa-operator")
-    tkn pipeline start operator \
-      --param repo-url=https://github.com/noobaa/noobaa-operator \
-      --param repo-ref=master \
-      --param base-image-registry=$BASE_IMAGE_REGISTRY \
-      --param image-name=noobaa-operator \
-      --param image-version=dev \
-      --param channel=$CHANNEL \
-      --param default-channel=$DEFAULT_CHANNEL \
-      --workspace name=workspace,claimName=noobaa-operator-volume \
-      --workspace name=patches,config=noobaa-operator-patch \
-      --pod-template pod-template.yaml \
-      -n okd-team
+    build_operator https://github.com/noobaa/noobaa-operator master noobaa-operator
     ;;
   "logging-view-plugin")
-    tkn pipeline start operand \
-      --param repo-url=https://github.com/openshift/logging-view-plugin \
-      --param repo-ref=main \
-      --param base-image-registry=$BASE_IMAGE_REGISTRY \
-      --param image-name=logging-view-plugin \
-      --param image-version=dev \
-      --workspace name=workspace,claimName=logging-view-plugin-volume \
-      --workspace name=patches,config=logging-view-plugin-patch \
-      --pod-template pod-template.yaml \
-      -n okd-team
+    build_operand https://github.com/openshift/logging-view-plugin main logging-view-plugin
     ;;
   "log-file-metric-exporter")
-    tkn pipeline start operand \
-      --param repo-url=https://github.com/ViaQ/log-file-metric-exporter \
-      --param repo-ref=release-5.8 \
-      --param base-image-registry=$BASE_IMAGE_REGISTRY \
-      --param image-name=log-file-metric-exporter \
-      --param image-version=dev \
-      --workspace name=workspace,claimName=log-file-metric-exporter-volume \
-      --workspace name=patches,config=log-file-metric-exporter-patch \
-      --pod-template pod-template.yaml \
-      -n okd-team
+    build_operand https://github.com/ViaQ/log-file-metric-exporter release-5.8 log-file-metric-exporter
+    ;;
+  "ocs-operator")
+    build_operator https://github.com/red-hat-storage/ocs-operator main ocs-operator
     ;;
   *)
-    echo "Usage: $0 <gitops-console-plugin|gitops-backend|gitops-operator>"
+    echo "Usage: $0 <operand/operator name>"
     exit 1
     ;;
 esac
